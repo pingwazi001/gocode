9ba795519a3916c5c9c6176cf64b206fa8bd8711
pingwazi <pingwazi521@163.com>
2022-01-13 19:39:40 +0800

    update code

--- src/main.go
+++ src/main.go
@@ -6,9 +6,10 @@ import (
 	"time"
 )
 
+//一个http请求  响应一个时间
 func main() {
 	http.HandleFunc("/time", func(rw http.ResponseWriter, r *http.Request) {
-		timeStr := time.Now().In(time.FixedZone("CST", 8*3600)).Format("2006-01-02 15:04:05")
+		timeStr := time.Now().In(time.FixedZone("CST", 8*3600)).Format("2006-01-02 15:04:05") + " 测试内容"
 		fmt.Println(timeStr)
 		rw.Write([]byte(timeStr))
 	})
